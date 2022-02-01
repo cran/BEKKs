6455022917a6fbf4752034b72fa2bf6609c832f6
Alexander Lange <alexander.lange@uni-goettingen.de>
2022-02-01 15:00:29 +0000

    version 1.0.1

---
+++ src/IndicatorFunction.cpp
@@ -0,0 +1,21 @@
+#include <RcppArmadillo.h>
+
+#include "IndicatorFunctions.h"
+
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp11)]]
+
+// [[Rcpp::export]]
+int indicatorFunction(arma::mat r, arma::mat signs){
+  r = r.t();
+
+  int indicator = 1;
+  int n = r.n_rows;
+  for (int i = 0; i<n; i++){
+    if(arma::as_scalar(signs.row(i)) * arma::as_scalar(r.row(i)) < 0){
+      indicator = 0;
+    }
+  }
+  return indicator;
+}
